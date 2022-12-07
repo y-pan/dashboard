3320ab570174fefab36f7bb124c7c86eff6bceba
Yun Kui Pan <yun.pan@pinchfinancial.com>
2022-12-07 14:07:16 -0500

    update with index

--- src/io/ports.ts
+++ src/io/ports.ts
@@ -1,22 +1,27 @@
 import { runAsync, RunResult } from "./run";
 const config = require("../config.json");
 
-
 export async function getPorts(isDownOnly = false): Promise<RunResult> {
   const result: RunResult = { stderr: "", stdout: "" };
 
+  let i = 0;
   for (let { name, port, group = "" } of config.portItems) {
     const { stderr, stdout } = await runAsync(`lsof -ti:${port}`);
 
     if (stdout) {
       // the port is in use
       if (!isDownOnly) {
-        result.stdout += `[${group}]\t[  up  ]\t${name} -> ${port}\tPid=${String(stdout).replaceAll("\n", " ").trim()}\n`;
+        result.stdout += `${
+          i + 1
+        }\t[${group}]\t[  up  ]\t${name} -> ${port}\tPid=${String(stdout)
+          .replaceAll("\n", " ")
+          .trim()}\n`;
       }
     } else {
       // the port is not in use
-      result.stdout +=   `[${group}]\t[ down ]\t${name} -> ${port}\n`;
+      result.stdout += `${i + 1}\t[${group}]\t[ down ]\t${name} -> ${port}\n`;
     }
+    i++;
   }
 
   return result;
