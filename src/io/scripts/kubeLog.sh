84444a01ac678fae1f6eabce2568de4b9e0d1699
Yun Kui Pan <yun.pan@pinchfinancial.com>
2023-04-05 17:41:31 -0400

    dashboard: update

--- src/io/scripts/kubeLog.sh
+++ src/io/scripts/kubeLog.sh
@@ -7,10 +7,14 @@ if [ -z "$namespace" ]; then
 fi
 
 timestamp=$(date +"%Y%m%d_%H%M%S")
-outdir=~/Downloads/kubelog_${timestamp}_${namespace}
+outdir=~/Downloads/kubelogs/kubelog_${timestamp}_${namespace}
 mkdir -p $outdir
 
-echo "outdir: $outdir namespace: $namespace, service name: ${serviceName} "
+echo "[outdir]: \n\t$outdir"
+echo "[namespace]: \n\t$namespace"
+echo "[service name]: \n\t${serviceName}"
+
+echo "\n\n"
 
 # discard the top line (header), take first column which is the pod name.
 # podNames=`kubectl get pods -n "$namespace" | tail -n +2 | grep "${serviceName}" | awk '{print $1}'`
@@ -20,9 +24,9 @@ logPod() {
   _podname=$1
 
   outfile=$outdir/$namespace.$_podname.log
-  echo "Logging starts: ${namespace}.${_podname}  > $outfile"
+  echo "Logging starts: ${namespace}.${_podname}  > \n\t$outfile"
   kubectl logs $_podname -n $namespace >$outfile
-  echo "Done"
+  echo "\tDone\n"
 }
 
 for podName in $podNames; do
