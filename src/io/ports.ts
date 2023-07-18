9528d50d6514eb28e8e70ff8d10aed3dd68c679f
Yun Kui Pan <yun.pan@pinchfinancial.com>
2023-07-18 19:50:25 -0400

    update

--- src/io/ports.ts
+++ src/io/ports.ts
@@ -5,28 +5,34 @@ export async function getPorts(isDownOnly = false): Promise<RunResult> {
   const result: RunResult = { stderr: "", stdout: "" };
 
   let i = 0;
-  for (let { name, port, group = "" } of config.portItems) {
+  for (let { name, port, group = "", note = "" } of config.portItems) {
     if (port == undefined) {
       // treat it as a separator to render a empty line
-      result.stdout += "\n";
+      result.stdout +=
+        "= == === ===== ======= ============ =================== ===============================\n";
       continue;
     }
+    i++;
     const { stderr, stdout } = await runAsync(`lsof -ti:${port}`);
 
-    if (stdout) {
-      // the port is active
-      if (!isDownOnly) {
-        result.stdout += `${
-          i + 1
-        }\t[${group}]\t[  up  ]\t${name} -> ${port}\tPid=${String(stdout)
-          .replace(/\n/g, " ")
-          .trim()}\n`;
-      }
-    } else {
-      // the port is inactive
-      result.stdout += `${i + 1}\t[${group}]\t[ down ]\t${name} -> ${port}\n`;
+    const isPortUp = !!stdout;
+
+    if (isDownOnly && isPortUp) {
+      continue;
     }
-    i++;
+
+    if (isPortUp) {
+      const out = String(stdout).replace(/\n/g, " ").trim();
+      result.stdout += `${i}\t[${group}]\t[  up  ]\t${name} -> ${port}\tPid=${out}\n`;
+      // command to kill port
+      result.stdout += `\t\t\t\tkill $(lsof -ti:${port})\n`;
+    } else {
+      result.stdout += `${i}\t[${group}]\t[ down ]\t${name} -> ${port}\n`;
+    }
+
+    if (note) {
+      result.stdout += `${note}\n`;
+    }
   }
 
   return result;
